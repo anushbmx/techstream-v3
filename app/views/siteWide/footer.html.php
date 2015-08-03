a40123d387c39b985a01ee8028922acbc20d4e31
Anush <anush@techstream.org>
2015-08-03 20:55:12 +0530

    Updated footer content

--- app/views/siteWide/footer.html.php
+++ app/views/siteWide/footer.html.php
@@ -17,9 +17,27 @@
 				</ul>
 			</div>
 		</div>
-		<div class="row">
-			<div class="column small-12 medium-10 small-centered push-4-top">
+		<div class="row push-2-top push-2-bottom">
+			<div class="column small-12 medium-10 small-centered">
 				<ul class="inline-list nospace friendLinks text-center">
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 1</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="row push-2-bottom">
+			<div class="column small-6">
+				<ul class="inline-list nospace">
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 1</a></li>
+					<li><a href="">Link 1</a></li>
+				</ul>
+			</div>
+			<div class="column small-6">
+				<ul class="inline-list nospace right">
 					<li><a href="">Link 1</a></li>
 					<li><a href="">Link 1</a></li>
 					<li><a href="">Link 1</a></li>
