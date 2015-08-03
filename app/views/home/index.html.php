a7f367e7db4261dd555757953a86a842b51f4d43
Anush <anush@techstream.org>
2015-08-03 21:41:14 +0530

    Added content to Index Page

--- app/views/home/index.html.php
+++ app/views/home/index.html.php
@@ -1,1 +1,56 @@
-Hello <?=$data['username'] ?>
+<section id="content">
+	<div class="featured-post border bb">
+		<div class="row">
+			<div class="column small-12 small-centered medium-uncentered medium-10">
+				<h1>Main post</h1>
+			</div>
+			<div class="column small-12 text-center medium-2">
+				<h1>Ads</h1>
+			</div>
+		</div>	
+	</div>
+	<div class="second-post">
+		<div class="row">
+			<div class="column medium-8 small-12 border br">
+				<h1>Second Articles</h1>
+			</div>
+			<div class="column medium-4 show-for-medium-up">
+				<h1>Bits</h1>
+			</div>
+		</div>
+	</div>
+	<div class="third-post">
+		<div class="row border bt bb">
+			<div class="column small-12 medium-3">
+				<h4>Article 1</h4>
+			</div>
+			<div class="column small-12 medium-3">
+				<h4>Article 2</h4>
+			</div>
+			<div class="column small-12 medium-3">
+				<h4>Article 3</h4>
+			</div>
+			<div class="column small-12 medium-3">
+				<h4>Article 4</h4>
+			</div>
+		</div>
+	</div>
+	<div class="most-read">
+		<div class="row border bt bb">
+			<div class="column small-12 medium-4">
+				<h4>Section 1</h4>
+			</div>
+			<div class="column small-12 medium-8 border bl">
+				<h4>Most Read 2</h4>
+				<div class="row">
+					<div class="column medium-6 border br">
+						<h4>Article</h4>
+					</div>
+					<div class="column medium-6 border show-for-medium-up">
+						<h4>Bits</h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
