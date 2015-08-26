15774de114eca2a407f627e392cc34bd341e45f6
Anush <anush@techstream.org>
2015-08-27 00:47:54 +0530

    Completed Pages

---
+++ app/views/home/page.html.php
@@ -0,0 +1,24 @@
+<section id="content">
+	<div class="article-head">
+		<div class="row">
+			<div class="column small-12 medium-8 large-8">
+				<h1>Article Title</h1>
+				<p class="border bt">Description</p>
+			</div>
+			<div class="column small-6 medium-4 large-4 small-centered medium-uncentered">
+				<h4>Ads</h4>
+			</div>
+		</div>
+	</div>
+
+	<div class="article-content">
+		<div class="row">
+			<div class="column small-12 medium-8 large-8 border br">
+				<h1>Article</h1>
+			</div>
+			<div class="column small-12 medium-4 large-4">
+				<h5>Sidebar</h5>
+			</div>
+		</div>
+	</div>
+</section>
