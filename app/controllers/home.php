d79007c82dcac151a3cec211d778ece473e45c72
Anush <anush@techstream.org>
2015-08-26 22:10:20 +0530

    Completed Article Template

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -7,9 +7,16 @@ class Home extends Controller
 {
 	public function index( $paramater = null )
 	{
-		$data['username'] = "Anush";
-		$data['TITLE'] = "it works";
-		$this->view('home/index.html',$data);
+		if( ! $paramater ){
+			$data['username'] = "Anush";
+			$data['TITLE'] = "it works";
+			$this->view('home/index.html',$data);
+		} else {
+			$data['username'] = "Anush";
+			$data['TITLE'] = "Article";
+			$this->view('home/article.html',$data);
+		}
+		
 	}
 
 }
---
+++ app/views/home/article.html.php
@@ -0,0 +1,40 @@
+<section id="content">
+	<div class="article-head">
+		<div class="row">
+			<div class="column small-12 medium-9 large-8 small-centered">
+				<small>Article image</small>
+				<h1>Article Title</h1>
+				<ul class="inline-list border bt">
+					<li>Date</li>
+					<li>Section</li>
+					<li>Comments</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="article-content">
+		<div class="row">
+			<div class="column small-8 medium-4 large-uncentered small-centered large-2">
+				<h6>Ads</h6>
+			</div>
+			<div class="column small-12 medium-9 large-8 large-uncentered small-centered">
+				<p>Article is here</p>
+			</div>
+			<div class="column large-2 hide-for-medium-down">
+				<h6>Social Links</h6>
+			</div>
+		</div>
+	</div>
+
+	<div class="article-discussion border bt">
+		<div class="row">
+			<div class="column small-12 medium-8 large-8 border br">
+				<h1>Comments</h1>
+			</div>
+			<div class="column small-12 medium-4 large-4">
+				<h5>Sidebar</h5>
+			</div>
+		</div>
+	</div>
+</section>
