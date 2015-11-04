309c7ef79ae941415f6c212f7dca6fdf1809b3cb
Anush <anush@techstream.org>
2015-11-04 16:14:31 +0530

    reduced size of article post to 1 word in edit post

--- app/controllers/admin.php
+++ app/controllers/admin.php
@@ -154,7 +154,7 @@ class Admin extends Controller
 					'title'		=> array(
 						'name'	   => 'Post title',
 						'required' => true,
-						'min' 	   => 1
+						'min' 	   => 5
 					),
 					'description'   => array(
 						'name'		=> 'description',
@@ -247,7 +247,7 @@ class Admin extends Controller
 					'title'		=> array(
 						'name'	   => 'Post title',
 						'required' => true,
-						'min' 	   => 10
+						'min' 	   => 5
 					),
 					'description'   => array(
 						'name'		=> 'description',
