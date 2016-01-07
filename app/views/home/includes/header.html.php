95c09e2af8214f045a8d2a53c4c0eb14f1521cb1
Anush <anush@techstream.org>
2016-01-07 16:22:29 +0530

    Added Search

--- app/views/home/includes/header.html.php
+++ app/views/home/includes/header.html.php
@@ -2,10 +2,19 @@
 <div id="container">
 	<section id="header">
 		<div class="row">
-			<div class="column small-12">
+			<div class="column small-12 medium-6">
 				<div class="row">
 					<div class="column small-4 medium-3 large-2 small-centered medium-uncentered logo">
 						<a href="<?=PUBLICPATH?>"></a>
+					</div>
+				</div>
+			</div>
+			<div class="column small-12 medium-6">
+				<div class="row">
+					<div class="column small-12 right medium-6 padd-2-top">
+						<form class="search-box" action="<?=PUBLICPATH?>search">
+							<input placeholder="Search in Tech Stream" type="text" name="q" autocomplete="off">
+						</form>
 					</div>
 				</div>
 			</div>
