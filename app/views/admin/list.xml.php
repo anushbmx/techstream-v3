a3a28cb62f667b6a763182cdb2c6d013d21e4966
Anush A <anush@techstream.org>
2023-12-20 19:46:52 +0530

    Updated XML

--- app/views/admin/list.xml.php
+++ app/views/admin/list.xml.php
@@ -34,92 +34,130 @@
 		<wp:wxr_version>1.2</wp:wxr_version>
 		<wp:base_site_url>https://techstream.org</wp:base_site_url>
 		<wp:base_blog_url>https://techstream.org</wp:base_blog_url>
-
 		<wp:author><wp:author_id>1</wp:author_id><wp:author_login><![CDATA[anush]]></wp:author_login><wp:author_email><![CDATA[anush@techstream.org]]></wp:author_email><wp:author_display_name><![CDATA[Anush]]></wp:author_display_name><wp:author_first_name><![CDATA[]]></wp:author_first_name><wp:author_last_name><![CDATA[]]></wp:author_last_name></wp:author>
-
 		<generator>https://wordpress.org/?v=6.1.1</generator>
-
 		<?php 
 			for ( $i = 0; $i<sizeof($data['items']); $i++ ) {
 		?>
 			<item>
-					<title><![CDATA[<?=$data['items'][$i]['TITLE']?>]]></title>
-					<link>https://techstream.ord/Web-Development/Boost-the-Design-of-Sports-Team-Website/</link>
-					<pubDate>Mon, 22 Jun 2020 07:52:45 +0000</pubDate>
-					<dc:creator><![CDATA[anush]]></dc:creator>
-					<description><![CDATA[<?=$data['items'][$i]['DES']?>]]></description>
-					<content:encoded><![CDATA[<?=htmlspecialchars_decode($data['items'][$i]['CONTENT'])?>]]></content:encoded>
-					<wp:post_id>131</wp:post_id>
-					<wp:comment_status><![CDATA[closed]]></wp:comment_status>
-					<wp:ping_status><![CDATA[open]]></wp:ping_status>
-					<wp:status><![CDATA[publish]]></wp:status>
-					<wp:post_type><![CDATA[post]]></wp:post_type>
-					<wp:is_sticky>0</wp:is_sticky>
-					<category domain="category" nicename="plus-one"><![CDATA[<?=$data['items'][$i]['SUBSEC']?>]]></category>
-					</wp:postmeta>
-					<wp:postmeta>
-						<wp:meta_key><![CDATA[_yoast_wpseo_metadesc]]></wp:meta_key>
-						<wp:meta_value><![CDATA[<?=$data['items'][$i]['DES']?>]]></wp:meta_value>
-					</wp:postmeta>
-										
-				</item>			
+				<title><![CDATA[<?=$data['items'][$i]['TITLE']?>]]></title>
+				<link><?=PUBLICPATH . $data['LINK']?></link>
+				<pubDate><?=$data['DATE']?>" <?php else : ?> value="<?=date("Y-m-d  H:i:s",time())?> +0000</pubDate>
+				<dc:creator>
+					<![CDATA[admin]]>
+				</dc:creator>
+				<description></description>
+				<content:encoded>
+					<![CDATA[<?=htmlspecialchars_decode($data['items'][$i]['CONTENT'])?>:]]>
+				</content:encoded>
+				<excerpt:encoded>
+					<![CDATA[]]>
+				</excerpt:encoded>
+				<wp:post_id>6668</wp:post_id>
+				<wp:post_date>
+					<![CDATA[<?=$data['DATE']?>" <?php else : ?> value="<?=date("Y-m-d  H:i:s",time())?>]]>
+				</wp:post_date>
+				<wp:post_date_gmt>
+					<![CDATA[<?=$data['DATE']?>" <?php else : ?> value="<?=date("Y-m-d  H:i:s",time())?>]]>
+				</wp:post_date_gmt>
+				<wp:post_modified>
+					<![CDATA[<?=$data['DATE']?>" <?php else : ?> value="<?=date("Y-m-d  H:i:s",time())?>]]>
+				</wp:post_modified>
+				<wp:post_modified_gmt>
+					<![CDATA[<?=$data['DATE']?>" <?php else : ?> value="<?=date("Y-m-d  H:i:s",time())?>]]>
+				</wp:post_modified_gmt>
+				<wp:comment_status>
+					<![CDATA[closed]]>
+				</wp:comment_status>
+				<wp:ping_status>
+					<![CDATA[closed]]>
+				</wp:ping_status>
+				<wp:post_name>
+					<![CDATA[<?=$data['LINK']?>]]>
+				</wp:post_name>
+				<wp:status>
+					<![CDATA[publish]]>
+				</wp:status>
+				<wp:post_parent>0</wp:post_parent>
+				<wp:menu_order>0</wp:menu_order>
+				<wp:post_type>
+					<![CDATA[post]]>
+				</wp:post_type>
+				<wp:post_password>
+					<![CDATA[]]>
+				</wp:post_password>
+				<wp:is_sticky>0</wp:is_sticky>
+				<category domain="category" nicename="<?=($data['SECURL'])?>">
+					<![CDATA[<?=removeHyphen($data['SECURL'])?>]]>
+				</category>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[_edit_last]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[1]]>
+					</wp:meta_value>
+				</wp:postmeta>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[_yoast_wpseo_content_score]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[60]]>
+					</wp:meta_value>
+				</wp:postmeta>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[_yoast_wpseo_primary_category]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[3]]>
+					</wp:meta_value>
+				</wp:postmeta>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[_yoast_wpseo_focuskw]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[physics textbook]]>
+					</wp:meta_value>
+				</wp:postmeta>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[_yoast_wpseo_title]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[%%title%%]]>
+					</wp:meta_value>
+				</wp:postmeta>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[_yoast_wpseo_metadesc]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[<?=$data['items'][$i]['DES']?>]]>
+					</wp:meta_value>
+				</wp:postmeta>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[_yoast_wpseo_linkdex]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[51]]>
+					</wp:meta_value>
+				</wp:postmeta>
+				<wp:postmeta>
+					<wp:meta_key>
+						<![CDATA[classic-editor-remember]]>
+					</wp:meta_key>
+					<wp:meta_value>
+						<![CDATA[classic-editor]]>
+					</wp:meta_value>
+				</wp:postmeta>
+			</item>
 		<?php
 			}
 		?>
-
-		<item>
-			<title><![CDATA[9 Ways to Boost the Design of Your Sports Team Website]]></title>
-			<link>https://techstream.ord/Web-Development/Boost-the-Design-of-Sports-Team-Website/</link>
-			<pubDate>Mon, 22 Jun 2020 07:52:45 +0000</pubDate>
-			<dc:creator><![CDATA[anush]]></dc:creator>
-			<description><![CDATA[Your club’s or sports team’s official website is a pivotal part of online real estate. It serves as the public face of your team or club and is likely to be the very first place people go to learn more about you. It should ideally enlighten your followers, draw in new ones, and include the most recent information on your activities, victories, defeats, and other events.]]></description>
-			<content:encoded><![CDATA[]]></content:encoded>
-			<excerpt:encoded><![CDATA[]]></excerpt:encoded>
-			<wp:post_id>131</wp:post_id>
-			<wp:post_date><![CDATA[2020-06-22 07:52:45]]></wp:post_date>
-			<wp:post_date_gmt><![CDATA[2020-06-22 07:52:45]]></wp:post_date_gmt>
-			<wp:post_modified><![CDATA[2023-01-22 14:32:17]]></wp:post_modified>
-			<wp:post_modified_gmt><![CDATA[2023-01-22 14:32:17]]></wp:post_modified_gmt>
-			<wp:comment_status><![CDATA[closed]]></wp:comment_status>
-			<wp:ping_status><![CDATA[open]]></wp:ping_status>
-			<wp:post_name><![CDATA[kerala-plus-one-urdu-textbook-pdf-download]]></wp:post_name>
-			<wp:status><![CDATA[publish]]></wp:status>
-			<wp:post_parent>0</wp:post_parent>
-			<wp:menu_order>0</wp:menu_order>
-			<wp:post_type><![CDATA[post]]></wp:post_type>
-			<wp:post_password><![CDATA[]]></wp:post_password>
-			<wp:is_sticky>0</wp:is_sticky>
-											<category domain="category" nicename="plus-one"><![CDATA[Plus One]]></category>
-							<wp:postmeta>
-			<wp:meta_key><![CDATA[classic-editor-remember]]></wp:meta_key>
-			<wp:meta_value><![CDATA[classic-editor]]></wp:meta_value>
-			</wp:postmeta>
-								<wp:postmeta>
-			<wp:meta_key><![CDATA[_edit_last]]></wp:meta_key>
-			<wp:meta_value><![CDATA[1]]></wp:meta_value>
-			</wp:postmeta>
-								<wp:postmeta>
-			<wp:meta_key><![CDATA[_wp_page_template]]></wp:meta_key>
-			<wp:meta_value><![CDATA[default]]></wp:meta_value>
-			</wp:postmeta>
-								<wp:postmeta>
-			<wp:meta_key><![CDATA[_yoast_wpseo_content_score]]></wp:meta_key>
-			<wp:meta_value><![CDATA[30]]></wp:meta_value>
-			</wp:postmeta>
-								<wp:postmeta>
-			<wp:meta_key><![CDATA[_yoast_wpseo_primary_category]]></wp:meta_key>
-			<wp:meta_value><![CDATA[7]]></wp:meta_value>
-			</wp:postmeta>
-								<wp:postmeta>
-			<wp:meta_key><![CDATA[_yoast_wpseo_title]]></wp:meta_key>
-			<wp:meta_value><![CDATA[%%title%%]]></wp:meta_value>
-			</wp:postmeta>
-								<wp:postmeta>
-			<wp:meta_key><![CDATA[_yoast_wpseo_metadesc]]></wp:meta_key>
-			<wp:meta_value><![CDATA[Your club’s or sports team’s official website is a pivotal part of online real estate. It serves as the public face of your team or club and is likely to be the very first place people go to learn more about you. It should ideally enlighten your followers, draw in new ones, and include the most recent information on your activities, victories, defeats, and other events.]]></wp:meta_value>
-			</wp:postmeta>
-							
-			</item>
 	</channel>
 </rss>
 </textarea>
