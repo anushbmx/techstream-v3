67cda78bfca67920d7ad7a59faf8ceeebc205606
Anush A <anush@techstream.org>
2024-01-01 13:36:51 +0530

    Updated export settings

--- app/views/admin/list.xml.php
+++ app/views/admin/list.xml.php
@@ -2,7 +2,7 @@ ID, Title, Category, Sub Sec, Link, Date, Description, Content
 <?php 
 	for ( $i = 0; $i<sizeof($data['items']); $i++ ) {
 ?>
-	<?=$data['items'][$i]['SL_NO']?>, <?=$data['items'][$i]['TITLE']?>, <?=$data['items'][$i]['SEC']?>, <?=$data['items'][$i]['SUBSEC']?>, <?=PUBLICPATH . $data['items'][$i]['LINK']?>, <?=$data['DATE']?>, <?=$data['items'][$i]['DES']?>, <?=htmlspecialchars_decode($data['items'][$i]['CONTENT'])?>,
+	<?=$data['items'][$i]['SL_NO']?>, <?=$data['items'][$i]['TITLE']?>, <?=$data['items'][$i]['SEC']?>, <?=$data['items'][$i]['SUBSEC']?>, <?=PUBLICPATH . $data['items'][$i]['LINK']?>, <?=$data['DATE']?>, <?=$data['items'][$i]['DES']?>, "<?=htmlspecialchars_decode($data['items'][$i]['CONTENT'])?>",
 <?php
 	}
 ?>
