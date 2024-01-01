d270a9222b162f2a9556c3464534be4d33e0ea98
Anush A <anush@techstream.org>
2024-01-01 14:24:54 +0530

    Updated xml

--- app/views/admin/list.xml.php
+++ app/views/admin/list.xml.php
@@ -6,13 +6,13 @@
 ?>
 <record>
 	<ID><![CDATA[<?=$data['items'][$i]['SL_NO']?>]]></ID>
-	<post_title><![CDATA[<?=$data['items'][$i]['TITLE']?>]]></title>
+	<post_title><![CDATA[<?=$data['items'][$i]['TITLE']?>]]></post_title>
 	<section><![CDATA[<?=$data['items'][$i]['SEC']?>]]></section>
 	<subsection><![CDATA[<?=$data['items'][$i]['SUBSEC']?>]]></subsection>
-	<guid><![CDATA[<?=PUBLICPATH . $data['items'][$i]['LINK']?>]]></item>
-	<post_date><![CDATA[<?=$data['items'][$i]['DATE']?>]]></date>
-	<post_excerpt><![CDATA[<?=trim($data['items'][$i]['DES'])?>]]></description>
-	<post_content><![CDATA[<?=htmlspecialchars_decode($data['items'][$i]['CONTENT'])?>]]></content>
+	<guid><![CDATA[<?=PUBLICPATH . $data['items'][$i]['LINK']?>]]></guid>
+	<post_date><![CDATA[<?=$data['items'][$i]['DATE']?>]]></post_date>
+	<post_excerpt><![CDATA[<?=trim($data['items'][$i]['DES'])?>]]></post_excerpt>
+	<post_content><![CDATA[<?=htmlspecialchars_decode($data['items'][$i]['CONTENT'])?>]]></post_content>
 </record>
 <?php
 }
