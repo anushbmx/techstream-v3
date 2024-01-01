957302b9676c514e5aadf17c3cfe0064fb46028d
Anush A <anush@techstream.org>
2024-01-01 13:41:30 +0530

    reformatted codebase

--- app/views/admin/list.xml.php
+++ app/views/admin/list.xml.php
@@ -1,10 +1,11 @@
 <textarea name="content" cols="77" rows="30">
-	ID, Title, Category, Sub Sec, Link, Date, Description, Content
+ID, Title, Category, Sub Sec, Link, Date, Description, Content
 <?php 
 	for ( $i = 0; $i<sizeof($data['items']); $i++ ) {
 ?>
-	<?=$data['items'][$i]['SL_NO']?>, <?=$data['items'][$i]['TITLE']?>, <?=$data['items'][$i]['SEC']?>, <?=$data['items'][$i]['SUBSEC']?>, <?=PUBLICPATH . $data['items'][$i]['LINK']?>, <?=$data['DATE']?>, <?=$data['items'][$i]['DES']?>, "",
+<?=$data['items'][$i]['SL_NO']?>, <?=$data['items'][$i]['TITLE']?>, <?=$data['items'][$i]['SEC']?>, <?=$data['items'][$i]['SUBSEC']?>, <?=PUBLICPATH . $data['items'][$i]['LINK']?>, <?=$data['DATE']?>, <?=$data['items'][$i]['DES']?>, ""
+
 <?php
-	}
+}
 ?>
 </textarea>
