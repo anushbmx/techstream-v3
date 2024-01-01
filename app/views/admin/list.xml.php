3857b516cc7853e8c6c6cb9a613e740de9f8d2fb
Anush A <anush@techstream.org>
2024-01-01 13:39:47 +0530

    Removed content

--- app/views/admin/list.xml.php
+++ app/views/admin/list.xml.php
@@ -3,7 +3,7 @@
 <?php 
 	for ( $i = 0; $i<sizeof($data['items']); $i++ ) {
 ?>
-	<?=$data['items'][$i]['SL_NO']?>, <?=$data['items'][$i]['TITLE']?>, <?=$data['items'][$i]['SEC']?>, <?=$data['items'][$i]['SUBSEC']?>, <?=PUBLICPATH . $data['items'][$i]['LINK']?>, <?=$data['DATE']?>, <?=$data['items'][$i]['DES']?>, "<?=htmlspecialchars_decode($data['items'][$i]['CONTENT'])?>",
+	<?=$data['items'][$i]['SL_NO']?>, <?=$data['items'][$i]['TITLE']?>, <?=$data['items'][$i]['SEC']?>, <?=$data['items'][$i]['SUBSEC']?>, <?=PUBLICPATH . $data['items'][$i]['LINK']?>, <?=$data['DATE']?>, <?=$data['items'][$i]['DES']?>, "",
 <?php
 	}
 ?>
